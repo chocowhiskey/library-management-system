7b8ac2ffe455440825900dd2d8bbb6eb637ebeef
Simone <simohoff@gmx.de>
2026-01-26 10:52:27 +0100

    Add book entity

---
+++ src/main/java/com/library/management/domain/Book.java
@@ -0,0 +1,26 @@
+package com.library.management.domain;
+
+public class Book {
+    private final String isbn;
+    private String title;
+    private String author;
+    private String genre;
+    private boolean available;
+
+    public Book(String isbn, String title, String author, String genre) {
+        if (isbn == null || isbn.isBlank()) {
+            throw new IllegalArgumentException("ISBN cannot be null or blank");
+        }
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("title cannot be null or blank");
+        }
+        if (author == null || author.isBlank()) {
+            throw new IllegalArgumentException("author cannot be null or blank");
+        }
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.available = true; // By default, the book is available
+    }
+}
