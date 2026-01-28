31c55a3f5d5031f9147bd7dfd37ee734dcbc4c76
Simone <simohoff@gmx.de>
2026-01-28 20:33:18 +0100

    Add to book entity validation and business logic

--- src/main/java/com/library/management/domain/Book.java
+++ src/main/java/com/library/management/domain/Book.java
@@ -1,5 +1,7 @@
 package com.library.management.domain;
 
+import java.util.Objects;
+
 public class Book {
     private final String isbn;
     private String title;
@@ -42,5 +44,39 @@ public class Book {
 
     public boolean isAvailable() {
         return available;
+    }
+
+    public void markAsUnavailable() {
+        this.available = false;
+    }
+
+    public void markAsAvailable() {
+        this.available = true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Book book = (Book) o;
+        return Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", available=" + available +
+                '}';
     }
 }
